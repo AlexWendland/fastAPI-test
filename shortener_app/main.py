d6ac11b22f3d91049d5cf4788a4a829a47fbbed8
Alex Wendland <awendland@cryptocompare.com>
2023-03-05 20:23:17 +0000

    Add database and update url endpoint to write to this

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -4,16 +4,33 @@ This is the main file for running the fast API.
 Author: Alex Wendland
 """
 
+# This is used to generate random strings
+import secrets
+
 # This is used to validated URLS
 import validators
 
 # Main class to run a fast API.
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 
-# This imports our schemas
-from . import schemas
+# Used for tpying
+from sqlalchemy.orm import Session
+
+# This imports our API schemas and database models
+from . import models, schemas
+
+# This imports connections to our database.
+from .database import SessionLocal, engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # This will handle bad requests from the user.
 def raise_bad_request(message: str):
@@ -24,8 +41,26 @@ def raise_bad_request(message: str):
 def read_root():
     return "Hello World!"
 
-@app.post("/url")
-def create_url(url: schemas.URLBase):
+# This is the route to shorten a URL.
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+
     if not validators.url(url.target_url):
         raise_bad_request("Invalid URL")
-    return f"TODO: Create database entry for {url.target_url}"
+
+    # Select characters to make the key and secret key from
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+
+    # Add this URL to the database
+    db_url = models.URL(key=key, secret_key=secret_key, target_url=url.target_url)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+
+    # Update object to return to the user
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
---
+++ shortener_app/models.py
@@ -0,0 +1,23 @@
+"""
+This file contains the models for the shortener_app's database.
+
+Author: Alex Wendland
+"""
+
+# These are types for the data in the database.
+from sqlalchemy import Boolean, Column, Integer, String
+
+# This is the database class.
+from .database import Base
+
+# This is the model for the urls being stored in the database.
+class URL(Base):
+    # By default table names are plurals of the class name.
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    key = Column(String, unique=True, index=True)
+    secret_key = Column(String, unique=True, index=True)
+    target_url = Column(String, index=True)
+    is_active = Column(Boolean, default=True)
+    clicks = Column(Integer, default=0)
