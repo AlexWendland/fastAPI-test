5af18dda5bf4b61284edfd20737b1a0971ac4d4f
Alex Wendland <awendland@cryptocompare.com>
2023-03-05 18:29:21 +0000

    Setting up the enviroment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fastAPI-test
+# FastAPI test
+
+This project follows the [Real Python](https://realpython.com/) course: [URL shortener fast API](https://realpython.com/courses/url-shortener-fastapi/).
+
+I want to use this to learn setting up CRUD API's and use databases with python.
---
+++ shortener_app/config.py
@@ -0,0 +1,23 @@
+"""
+This module contains the configuration of the application.
+
+Author: Alex Wendland
+"""
+# This decorator caches the data.
+from functools import lru_cache
+
+# This is a library that allows us to use environment variables.
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./shortener.db"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Environment: {settings.env_name}")
+    return settings
